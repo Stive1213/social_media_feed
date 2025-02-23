7d5e5dc0cabbcfbdce61a55ee55b43aaa119fc09
stive1213 <estifanosamsalu833@gmail.com>
2025-02-23 00:11:51 -0800

    `Refactor models in social_media_feed/core/models.py`

--- social_media_feed/core/models.py
+++ social_media_feed/core/models.py
@@ -1,31 +1,30 @@
+# core/models.py
+
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class post(models.Model):
+class Post(models.Model):
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.content[:20]}..."
-    
-
-class comment(models.Model):
-    content = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(post, on_delete=models.CASCADE, related_name='comments')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.content[:20]}..."
 
-class like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
-    post = models.ForeignKey(post, on_delete=models.CASCADE, related_name='likes')
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-       return f"{self.user.username} liked {self.post.content[:20]}..."
-            
+        return f"{self.user.username} - {self.content[:20]}..."
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.content[:20]}..."
